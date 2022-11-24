66f9a1fd262b71de865d87a3a735f2cd221360cd
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-11-24 14:07:46 -0300

    melhoria: typer

--- js/script.js
+++ js/script.js
@@ -1,41 +1,27 @@
-const darkBtn = document.querySelector('input')
-const root = document.documentElement
+// window.sr = ScrollReveal({ reset: true })
 
-const lightTheme = {
-    '--main-color': '#0b5ed7',
-    '--negative-color': '#F39C12',
-    '--main-text-color': '#262626',
-    '--bio-text-color': '#FFFFFF',
-    '--bio-bg-color': '#333333',
-    '--bio-border-color': '#30363D',
-    '--about-bg-color': '#FFFFFF'
+// sr.reveal('.description', { duration: 2000, delay: 1000 })
+
+
+const el = document.querySelector('#digit')
+const text = "Desenvolvedor Full Stack."
+const interval = 100
+
+function showText(el, text, interval) {
+
+    const char = text.split("").reverse()
+    const typer = setInterval(() => {
+        
+        if(!char.length) {
+            return clearInterval(typer);
+        }
+
+        const next = char.pop()
+        
+        el.innerHTML += next
+
+    }, interval);
 }
 
-const darkTheme = {
-    '--main-color': '#F39C12',
-    '--negative-color': '#0b5ed7',
-    '--main-text-color': '#EEEEEE',
-    '--bio-text-color': '#EEEEEE',
-    '--bio-bg-color': '#363636',
-    '--bio-border-color': '#30363D',
-    '--about-bg-color': '#262626'
-}
+showText(el, text, interval)
 
-darkBtn.addEventListener('change', () => {
-    const check = darkBtn.checked
-    if(check) {
-        changeTheme(darkTheme)
-    } else {
-        changeTheme(lightTheme)
-    }
-})
-
-function changeTheme(theme) {
-    for(let prop in theme) {
-        changeProperty(prop, theme[prop])
-    }
-}
-
-function changeProperty(property, value) {
-    root.style.setProperty(property, value)
-}

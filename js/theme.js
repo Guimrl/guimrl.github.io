0d6931c2fdccf55cf296edcd147b4e3a9809f489
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-02 19:48:42 -0300

    update

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,5 @@
-const darkBtn = document.querySelector('input')
-const root = document.documentElement
+const input = document.querySelector('input');
+const root = document.documentElement;
 
 const lightTheme = {
     '--main-color': '#0b5ed7',
@@ -21,21 +21,33 @@ const darkTheme = {
     '--about-bg-color': '#262626'
 }
 
-darkBtn.addEventListener('change', () => {
-    const check = darkBtn.checked
-    if(check) {
-        changeTheme(darkTheme)
+input.addEventListener('change', () => {
+    const check = input.checked;
+    let GetTheme;
+    if (check) {
+        GetTheme = 'DARK';
+        changeTheme(darkTheme);
     } else {
-        changeTheme(lightTheme)
+        GetTheme = 'LIGHT';
+        changeTheme(lightTheme);
     }
+
+    //salva em localStorage
+    localStorage.setItem("PageTheme", JSON.stringify(GetTheme));
 })
 
 function changeTheme(theme) {
-    for(let prop in theme) {
-        changeProperty(prop, theme[prop])
+    for (let prop in theme) {
+        changeProperty(prop, theme[prop]);
     }
 }
 
 function changeProperty(property, value) {
-    root.style.setProperty(property, value)
+    root.style.setProperty(property, value);
+}
+
+let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
+
+if(GetTheme === 'DARK') {
+    changeTheme(darkTheme);
 }

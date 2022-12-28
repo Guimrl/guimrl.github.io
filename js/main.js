20cc60513d6fb31f52e333fc844fbb93d8ce6c72
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-28 14:46:05 -0300

    layout final nav/inicio/sobre

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-const openBtn = document.querySelector('.open-btn')
-const closeBtn = document.querySelector('.close-btn')
-const nav = document.querySelector('.nav')
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const nav = document.querySelector('.nav');
+const inicio = document.getElementById('inicio');
 
 openBtn.addEventListener('click', () => {
-    nav.classList.add('visivel')
+    nav.classList.add('visivel');
 })
 
 closeBtn.addEventListener('click', () => {
+    nav.classList.remove('visivel');
+})
+
+nav.addEventListener('click', () => {
     nav.classList.remove('visivel')
 })

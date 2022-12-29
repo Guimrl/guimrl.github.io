1c071abf049635199b2c0705703d57303c3964a6
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2022-12-29 00:28:01 -0300

    criando efeito smooth na navegação/blur do doc title

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
+// Efeito Smooth da navegação
+const menu = document.querySelectorAll('.navegacao a[href^="#"]');
+
+function descobreAdistanciaEntreASectionETopo(element) {
+    const id = element.getAttribute("href");
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollNativo(distanciaEntreASectionETopo) {
+    window.scroll({
+        top: distanciaEntreASectionETopo,
+        behavior: "smooth"
+    })
+}
+
+function scrollSessao(event) {
+    event.preventDefault();
+    const distanciaEntreASectionETopo = descobreAdistanciaEntreASectionETopo(event.target) - 90;
+    scrollNativo(distanciaEntreASectionETopo);
+}
+
+menu.forEach((link) => {
+    link.addEventListener("click", scrollSessao);
+})
+
+// Evento de click da Navbar
 const openBtn = document.querySelector('.open-btn');
 const closeBtn = document.querySelector('.close-btn');
 const nav = document.querySelector('.nav');
-const inicio = document.getElementById('inicio');
 
 openBtn.addEventListener('click', () => {
     nav.classList.add('visivel');
@@ -11,6 +36,16 @@ closeBtn.addEventListener('click', () => {
     nav.classList.remove('visivel');
 })
 
-nav.addEventListener('click', () => {
-    nav.classList.remove('visivel')
+menu.forEach((link) => {
+    link.addEventListener("click", () => nav.classList.remove('visivel'))
+})
+
+// Muda o title do HTML quando o usuario pressiona tab
+let title = document.title;
+window.addEventListener("blur", () => {
+    document.title = 'Volta aqui! :(';
+})
+
+window.addEventListener("focus", () => {
+    document.title = title;
 })

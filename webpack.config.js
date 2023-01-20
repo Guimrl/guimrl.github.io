403cf004a11737ae6cbcd642dd73d8987522d18c
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-01-20 11:49:32 -0300

    integrando babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
+
+
 module.exports = {
     entry: './src/js/main.js',
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js'
+    },
+    module:  {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
     }
 }

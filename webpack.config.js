ba5b58591bd86bfe2347a10dbc7f3fbaf11931d0
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-01-20 13:32:53 -0300

    gerando css pelo copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
     entry: './src/js/main.js',
     output: {
-        path: path.resolve(__dirname, '/dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         clean: true
-    },
-    module:  {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: 'src/index.html',
+            template: './src/index.html',
             hash: true
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/css', to: 'css' }
+            ]
         })
     ]
 }

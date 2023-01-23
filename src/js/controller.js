c5d2a693b173b66fc39eb195745e2bd17c7c969f
Guilherme Amaral <88007295+Guimrl@users.noreply.github.com>
2023-01-23 07:39:47 -0300

    separando os arquivos css em módulos

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,21 @@
-import '../css/style.css';
-import '../css/media.css';
+import '../css/document.css';
+import '../css/principal.css';
+import '../css/navbar.css';
+import '../css/root.css';
+import '../css/dark-mode.css';
+import '../css/scrollbar.css';
+import '../css/sobre.css';
+import '../css/botoes.css';
+import '../css/projetos.css';
+import '../css/footer.css';
+import '../css/media/media.css';
 
 import { title } from './title.js';
-import { getApi } from './apiGithub';
-import { navBar } from './navBar';
-import { navegacao } from './navegacao';
-import { digitaTexto } from './digitaTexto';
-import { tema } from './tema';
+import { getApi } from './apiGithub.js';
+import { navBar } from './navBar.js';
+import { navegacao } from './navegacao.js';
+import { digitaTexto } from './digitaTexto.js';
+import { tema } from './tema.js';
 
 const elemento = document.querySelector('#digit');
 const texto = "Desenvolvedor Full Stack.";

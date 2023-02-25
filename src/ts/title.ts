e1cf94954b656a6ac157d9911443d2c7b1b0b082
Guilherme Amaral <gui.amaral123@outlook.com>
2023-02-25 18:11:32 +0000

    add: title.ts

--- src/ts/title.ts
+++ src/ts/title.ts
@@ -1,21 +1,25 @@
-export function title() {
-    let title = document.title;
-    let counter = 0;
-    let interval = setInterval(tradeTitle, 2000);
-    let arrayTitle = [
+export function title(): void 
+{
+    let title: string = document.title;
+    let counter: number = 0;
+    let interval: any = setInterval(tradeTitle, 2000);
+    let arrayTitle: string[] = [
         "Desenvolvedor Back end",
         "Desenvolvedor Front end",
         "Desenvolvedor Full Stack",
         "Guilherme Amaral"
     ];
-    function tradeTitle() {
+
+    function tradeTitle(): void {
         document.title = arrayTitle[counter % arrayTitle.length];
         counter++;
     }
+
     window.addEventListener('focus', () => {
         document.title = title;
         interval = setInterval(tradeTitle, 2000);
     });
+
     window.addEventListener('blur', () => {
         document.title = 'Volta aqui! ;(';
         clearInterval(interval);

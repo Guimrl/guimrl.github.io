c9cedfe692edb26c4875cd999c2ae886b14506c4
Guilherme Amaral <gui.amaral123@outlook.com>
2023-02-28 16:05:38 +0000

    dist

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+import '../assets/css/document.css';
+import '../assets/css/principal.css';
+import '../assets/css/navbar.css';
+import '../assets/css/root.css';
+import '../assets/css/dark-mode.css';
+import '../assets/css/scrollbar.css';
+import '../assets/css/sobre.css';
+import '../assets/css/botoes.css';
+import '../assets/css/projetos.css';
+import '../assets/css/footer.css';
+import '../assets/css/modal.css';
+import '../assets/css/media/media.css';
+import { title } from './utils/title';
+import { getApi } from './services/apiGithub';
+import { navBar } from './views/navBar';
+import { scrollSmooth } from './utils/scrollSmooth';
+import { textDigit } from './utils/textDigit';
+import { theme } from './utils/theme';
+import { modal } from './views/modal';
+import { criaProjetos } from './views/criaProjetos';
+import { footer } from './views/footer';
+const element = document.querySelector('#digit');
+const text = "Desenvolvedor web Full Stack.";
+const interval = 100;
+title();
+getApi();
+navBar();
+scrollSmooth();
+if (element) {
+    textDigit(element, text, interval);
+}
+theme();
+modal();
+criaProjetos();
+footer();

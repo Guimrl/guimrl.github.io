3b8c066ed860ae52dcbd59228e44750d2f219a09
Guilherme Amaral <gui.amaral123@outlook.com>
2023-03-01 19:31:40 -0300

    delete src/js

--- src/js/views/footer.js
+++
@@ -1,9 +0,0 @@
-export function footer() {
-    let footer = document.querySelector("#footerTag");
-    if (footer) {
-        footer.innerHTML += `
-        <div class="footer">
-            <p>&copy;Guilherme<span class="highlight"> Amaral</span> - 2023</p>
-        </div>`;
-    }
-}

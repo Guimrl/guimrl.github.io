095e39f69b717bd7572a7d7005fd268dac901563
Guilherme Amaral <gui.amaral123@outlook.com>
2023-04-30 03:55:59 +0000

    create: controller

---
+++ src/controllers/Controller.ts
@@ -0,0 +1,79 @@
+import '../assets/css/document.css';
+import '../assets/css/principal.css';
+import '../assets/css/navbar.css';
+import '../assets/css/root.css';
+import '../assets/css/dark-mode.css';
+import '../assets/css/scrollbar.css';
+import '../assets/css/sobre.css';
+import '../assets/css/botoes.css';
+import '../assets/css/projetos.css';
+import '../assets/css/footer.css';
+import '../assets/css/modal.css';
+import '../assets/css/media/media.css';
+import '../assets/css/maintenance.css';
+
+import { createMaintenance } from '../views/createMaintenance';
+import { hideMaintenance } from '../views/maintenance';
+import { Progress } from '../utils/progress';
+import { Home } from '../views/Home';
+import { aboutSection } from '../views/aboutSection';
+import { createNav } from '../views/createNav';
+import { title } from '../utils/title';
+import { getApi } from '../services/apiGithub';
+import { NavBar } from '../views/NavBar';
+import { createModal } from '../views/createModal';
+import { scrollSmooth } from '../utils/scrollSmooth';
+import { textDigit } from '../utils/textDigit';
+import { theme } from '../utils/theme';
+import { Modal } from '../views/Modal';
+import { ApiProjects } from '../services/ApiProjects';
+import { createProjects } from '../views/createProjects';
+import { Footer } from '../views/Footer';
+
+export class Controller {
+
+    public render(): void {
+        const element: HTMLElement | null = document.querySelector('#digit');
+        const text: string = "Desenvolvedor web Full Stack.";
+        const interval: number = 100;
+
+        createMaintenance();
+
+        //onMaintenance();
+        hideMaintenance()
+
+        const progress: Progress = new Progress();
+        progress.scroll();
+
+        const home: Home = new Home();
+        home.createHome();
+
+        aboutSection();
+        createNav();
+        title();
+        getApi();
+
+        const nav: NavBar = new NavBar();
+        nav.togleNav();
+
+        createModal();
+        scrollSmooth();
+
+        if (element) {
+            textDigit(element, text, interval);
+        }
+
+        theme();
+
+        const modal: Modal = new Modal();
+        modal.togleModal();
+
+        const apiProjects: ApiProjects = new ApiProjects();
+        apiProjects.getData().then(projects => createProjects(projects));
+
+        const footer: Footer = new Footer();
+        footer.createFooter();
+
+    }
+
+}
